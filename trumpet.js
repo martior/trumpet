12199266ea21f7f2e4cc30421639dc2da208936a
Martin Opstad Reistadbakk <martior@jarn.com>
2011-12-23 00:10:31 +0100

    now use a hash fumction to dismiss message

--- trumpet.js
+++ trumpet.js
@@ -37,23 +37,58 @@
       }
     });
     
-    //code from: http://msdn.microsoft.com/en-us/library/ms537509(v=vs.85).aspx
-    function getInternetExplorerVersion()
-  // Returns the version of Internet Explorer or a -1
-  // (indicating the use of another browser).
-  {
-    var rv = -1; // Return value assumes failure.
-    if (navigator.appName == 'Microsoft Internet Explorer')
-    {
-      var ua = navigator.userAgent;
-      var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
-      if (re.exec(ua) != null)
-        rv = parseFloat( RegExp.$1 );
-    }
-    return rv;
-  }
-    
 
+	function murmurhash3_32_gc(key, seed) {
+		var remainder, bytes, h1, h1b, c1, c1b, c2, c2b, k1, i;
+
+		remainder = key.length & 3; // key.length % 4
+		bytes = key.length - remainder;
+		h1 = seed;
+		c1 = 0xcc9e2d51;
+		c2 = 0x1b873593;
+		i = 0;
+
+		while (i < bytes) {
+		  	k1 = 
+		  	  ((key.charCodeAt(i) & 0xff)) |
+		  	  ((key.charCodeAt(++i) & 0xff) << 8) |
+		  	  ((key.charCodeAt(++i) & 0xff) << 16) |
+		  	  ((key.charCodeAt(++i) & 0xff) << 24);
+			++i;
+
+			k1 = ((((k1 & 0xffff) * c1) + ((((k1 >>> 16) * c1) & 0xffff) << 16))) & 0xffffffff;
+			k1 = (k1 << 15) | (k1 >>> 17);
+			k1 = ((((k1 & 0xffff) * c2) + ((((k1 >>> 16) * c2) & 0xffff) << 16))) & 0xffffffff;
+
+			h1 ^= k1;
+	        h1 = (h1 << 13) | (h1 >>> 19);
+			h1b = ((((h1 & 0xffff) * 5) + ((((h1 >>> 16) * 5) & 0xffff) << 16))) & 0xffffffff;
+			h1 = (((h1b & 0xffff) + 0x6b64) + ((((h1b >>> 16) + 0xe654) & 0xffff) << 16));
+		}
+
+		k1 = 0;
+
+		switch (remainder) {
+			case 3: k1 ^= (key.charCodeAt(i + 2) & 0xff) << 16;
+			case 2: k1 ^= (key.charCodeAt(i + 1) & 0xff) << 8;
+			case 1: k1 ^= (key.charCodeAt(i) & 0xff);
+
+			k1 = (((k1 & 0xffff) * c1) + ((((k1 >>> 16) * c1) & 0xffff) << 16)) & 0xffffffff;
+			k1 = (k1 << 16) | (k1 >>> 16);
+			k1 = (((k1 & 0xffff) * c2) + ((((k1 >>> 16) * c2) & 0xffff) << 16)) & 0xffffffff;
+			h1 ^= k1;
+		}
+
+		h1 ^= key.length;
+
+		h1 ^= h1 >>> 16;
+		h1 = (((h1 & 0xffff) * 0x85ebca6b) + ((((h1 >>> 16) * 0x85ebca6b) & 0xffff) << 16)) & 0xffffffff;
+		h1 ^= h1 >>> 13;
+		h1 = ((((h1 & 0xffff) * 0xc2b2ae35) + ((((h1 >>> 16) * 0xc2b2ae35) & 0xffff) << 16))) & 0xffffffff;
+		h1 ^= h1 >>> 16;
+
+		return h1 >>> 0;
+	}
     function setup() {
         theme = doc.createElement('style');
         //theme.type = 'text/css';
@@ -72,11 +107,11 @@
           trumpetEl.id = 'trumpet_message';
           trumpetEl.className = 'trumpet';
           document.getElementsByTagName("body")[0].appendChild(trumpetEl);
-          message_dismissed = readCookie("trumpetapp_dm");
+          message_dismised = readCookie("trumpetapp_dm");
           on (trumpetEl,'click',function () {
               animate(0);
               message_dismissed = message;
-              createCookie("trumpetapp_dm",message);
+              createCookie("trumpetapp_dm",murmurhash3_32_gc(message,1));
           });
           setTimeout(showMessage,1000);
           
@@ -179,7 +214,7 @@
     
 
     showMessage = function(){
-        if (message_dismissed != message){
+        if (message_dismissed != murmurhash3_32_gc(message,1)){
             trumpetEl.innerHTML = message;
             animate(1);            
         }
