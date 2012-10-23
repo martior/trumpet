043c22863fb56f0105303d1360012144d9037f33
Martin Opstad Reistadbakk <martin@blaastolen.com>
2012-10-24 00:07:57 +0200

    updated build script for new file locations

--- src/build.py
+++ src/build.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 style = ""
-for line in open("src/stylesheets/trumpet.css"):
+for line in open("stylesheets/trumpet.css"):
   style += line.replace("\n"," ").replace("\t"," ").replace("  "," ")
   
 javascript = ""
-for line in open("src/javascripts/trumpet.js"):
+for line in open("javascripts/trumpet.js"):
     javascript += line.replace("STYLEHERE",style)
 
 
-f = open("public/javascripts/trumpet.js",'w')
+f = open("../public/javascripts/trumpet.js",'w')
 f.write(javascript)
